213fa0dc41bef339b2887232b81b917d7fb82847
minhengdey <dlminhanh272@gmail.com>
2025-04-19 19:59:23 +0700

    feat: get all by meetingId

--- Backend/ConnectMe/src/main/java/ttcs/connectme/repository/MeetingUserRepository.java
+++ Backend/ConnectMe/src/main/java/ttcs/connectme/repository/MeetingUserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ttcs.connectme.entity.MeetingUserEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,5 @@ public interface MeetingUserRepository extends JpaRepository<MeetingUserEntity, 
     Optional<MeetingUserEntity> findByIdAndIsDeletedFalse (Long id);
     boolean existsByMeetingIdAndUserIdAndIsDeletedFalse (Long meetingId, Long userId);
     Optional<MeetingUserEntity> findByMeetingIdAndUserIdAndIsDeletedFalse (Long meetingId, Long userId);
+    List<MeetingUserEntity> getAllByMeetingIdAndIsDeletedFalse (Long meetingId);
 }
