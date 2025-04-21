0275716d8494f855ccda953a64d5e4c18744e773
Duy Anh Nguyen <nguyenduyanh221204@gmail.com>
2025-04-21 16:58:44 +0700

    sos

--- Backend/ConnectMe/src/main/java/ttcs/connectme/repository/UserRepository.java
+++ Backend/ConnectMe/src/main/java/ttcs/connectme/repository/UserRepository.java
@@ -3,8 +3,12 @@ package ttcs.connectme.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ttcs.connectme.entity.UserEntity;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    Optional<UserEntity> findByUsername(String username);
 }
