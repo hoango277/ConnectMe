58ca278219cd934d8173d2c16cc575da95f55aed
Duy Anh Nguyen <nguyenduyanh221204@gmail.com>
2025-05-03 20:59:26 +0700

    Fix user repository, add logout and refresh token

--- Backend/ConnectMe/src/main/java/ttcs/connectme/configuration/SecurityConfig.java
+++ Backend/ConnectMe/src/main/java/ttcs/connectme/configuration/SecurityConfig.java
@@ -29,8 +29,8 @@ public class SecurityConfig {
     @Autowired
     private JwtCookieFilter jwtCookieFilter;
 
-    private final String[] PUBLIC_ENDPOINT_POST = {"/api/auth/**", "/api/user/**"};
-    private final String[] PUBLIC_ENDPOINT_GET = {};
+    private final String[] PUBLIC_ENDPOINT_POST = {"/api/auth/**"};
+    private final String[] PUBLIC_ENDPOINT_GET = {"/api/users/**"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
